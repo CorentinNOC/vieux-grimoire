7cfaadac40335fbd6cc5c11fffdc3f8da5902cd6
Corentin Nicolas <c.nicolas28210@gmail.com>
2025-12-11 15:49:49 +0100

    feat(books): add mongodb, routes,controllers,models features for books

---
+++ controllers/book.js
@@ -0,0 +1,18 @@
+const Book = require("../models/Book");
+
+exports.getBooks = (req, res, next) => {
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.createBook = (req, res, next) => {
+  delete req.body._id;
+  const book = new Book({
+    ...req.body,
+  });
+  book
+    .save()
+    .then(() => res.status(201).json({ message: "Livre enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
