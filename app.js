450c074e6dce29f75d54b423a489c44ad819f70a
CorentinNOC <c.nicolas28210@gmail.com>
2025-12-11 17:23:58 +0100

    feat(user): add user create and auth features (#2)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const app = express();
 
 const bookRoutes = require("./routes/book");
+const userRoutes = require("./routes/user");
 
 app.use(express.json());
 
@@ -26,5 +27,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/books", bookRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
