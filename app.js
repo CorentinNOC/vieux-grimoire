890bd48358357fe498730858cc782997c579e44e
CorentinNOC <c.nicolas28210@gmail.com>
2025-12-11 16:01:53 +0100

    Feat/basic books features (#1)
    * feat(books): add mongodb, routes,controllers,models features for books
    
    * feat(env): add dotenv packages to use .env

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+
+const bookRoutes = require("./routes/book");
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((err) => console.log("Connexion à MongoDB échouée !", err));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/books", bookRoutes);
+
+module.exports = app;
---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+userSchema.index({ email: 1 }, { unique: true });
+
+module.exports = mongoose.model("User", userSchema);
