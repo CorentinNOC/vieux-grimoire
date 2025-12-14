691f373a40a3966fa2e1b93d3b70e914cadbe63e
CorentinNOC <c.nicolas28210@gmail.com>
2025-12-14 11:22:21 +0100

    Feat/image optimization (#4)
    * feat(images): remove images from previous test & add image optimization
    
    * feat(images): remove image if modify

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,4 +1,6 @@
 const multer = require('multer');
+const sharp = require('sharp');
+const fs = require('fs');
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -13,8 +15,39 @@ const storage = multer.diskStorage({
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    callback(null, `${name}${Date.now()}.${extension}`);
   },
 });
 
-module.exports = multer({ storage: storage }).single('image');
+const upload = multer({ storage }).single('image');
+
+module.exports = (req, res, next) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!req.file) {
+      return next();
+    }
+
+    const filePath = req.file.path;
+    const tempPath = `${filePath}.tmp`;
+
+    sharp(filePath)
+      .resize(206, 260)
+      .jpeg({ quality: 80 })
+      .toFile(tempPath)
+      .then(() => {
+        fs.rename(tempPath, filePath, (fsErr) => {
+          if (fsErr) {
+            return next(fsErr);
+          }
+          next();
+        });
+      })
+      .catch((error) => {
+        next(error);
+      });
+  });
+};
